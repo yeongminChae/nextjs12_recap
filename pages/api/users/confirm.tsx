a7b228f33f21d7fa0bbb074b1c0a5a8f731bcbc8
Yeongmin_Chae <codudals98@naver.com>
2024-04-01 16:00:29 +0900

    쿠키 생성위해 iron-session 설치

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -2,14 +2,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { ResponseType } from "@libs/server/withHandler";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      token,
+    },
+    include: {
+      user: true,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.user.id,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrot-session",
+  password: "fdfsfdsfdsfsfdfsfdsfdsfsfdfsfdsfdsfsfdfsfdsfdsfsfdfsfdsfdsfs",
+});
