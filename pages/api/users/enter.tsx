a1c429c9f944329a34d6ccd564483a21f2cb8c0c
Yeongmin_Chae <codudals98@naver.com>
2024-04-01 10:23:43 +0900

    prisma client create 방법 1.

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,6 +3,43 @@ import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { email, phone } = req.body;
+  let user;
+
+  if (email) {
+    // client.user -> prisma가 자동으로 생성해준 client, 유저 테이블에 접근할 수 있음
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log("user", user);
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+        // string -> number로 변환
+      },
+    });
+    if (user) console.log("user", user);
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+  }
   return res.status(200);
 }
 
