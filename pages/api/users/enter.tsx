3c05a3ca0efb84db7dc47dd860201e8d98fbf0c2
Yeongmin_Chae <codudals98@naver.com>
2024-04-01 12:23:01 +0900

    토큰 랜덤 번호로 생성 + 코드 수정

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,30 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
+import { ResponseType } from "@libs/server/withHandler";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { email, phone } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(10000 + Math.random() * 90000).toString();
 
   const token = await client.token.create({
     data: {
-      token: "22",
+      token: payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: "Anonymous",
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
-  console.log(token);
 
-  return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
