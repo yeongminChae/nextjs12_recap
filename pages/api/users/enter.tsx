d48c05013ca17dcffd1e26051238713e220aa45b
Yeongmin_Chae <codudals98@naver.com>
2024-04-01 12:10:46 +0900

    Token 모델 추가
    connectOrCreate? where, create 메소드를 필수적으로 작성해야하고, 유저가 db에 있으면 토큰 발급, 없으면 db에 생성 + 토큰 발급을 동시에 진행해주는 메소드

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,17 +6,23 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
   const payload = phone ? { phone: +phone } : { email };
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      token: "22",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
 
   return res.status(200).end();
 }
