c7b90c76e9a83fd1d1d73ed382b0caa9dd55191e
Yeongmin_Chae <codudals98@naver.com>
2024-04-02 12:24:38 +0900

    useForm을 활용해 upload 폼 생성 및 useMutation, onValid 구현

---
+++ pages/products/upload.tsx
@@ -0,0 +1,73 @@
+import type { NextPage } from "next";
+import Button from "@components/button";
+import Input from "@components/input";
+import Layout from "@components/layout";
+import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+
+interface UploadProductsForm {
+  name: string;
+  price: number;
+  description: string;
+}
+
+const Upload: NextPage = () => {
+  const { register, handleSubmit } = useForm<UploadProductsForm>();
+  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+
+  const onValid = (data: UploadProductsForm) => {
+    if (loading) return;
+    uploadProduct(data);
+  };
+
+  return (
+    <Layout canGoBack title="Upload Product">
+      <form className="space-y-4 p-4" onSubmit={handleSubmit(onValid)}>
+        <div>
+          <label className="flex h-48 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-gray-600 hover:border-orange-500 hover:text-orange-500">
+            <svg
+              className="h-12 w-12"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input className="hidden" type="file" />
+          </label>
+        </div>
+        <Input
+          register={register("name", { required: true })}
+          name="name"
+          required
+          label="Name"
+          type="text"
+        />
+        <Input
+          register={register("price", { required: true })}
+          name="price"
+          required
+          label="Price"
+          type="text"
+          kind="price"
+        />
+        <TextArea
+          register={register("price", { required: true })}
+          required
+          name="description"
+          label="Description"
+        />
+        <Button text={loading ? "Loading..." : "Upload item"} />
+      </form>
+    </Layout>
+  );
+};
+
+export default Upload;
